8343ab8deffba8e2c7978d04694a866716f21060
Igor <guselnikov.uniikm@yandex.ru>
2022-07-19 21:27:40 +0500

    first task change

--- task1/Program.cs
+++ task1/Program.cs
@@ -25,13 +25,13 @@
 // }
 
 Console.WriteLine("Введите пятизначное число:");
-int palindrom = Convert.ToInt32(Console.ReadLine());
-int tenHundred = palindrom / 10000;
-int hundred = palindrom / 1000;
-int hundredCorrect = palindrom % 10;
-int thousend = palindrom / 100;
-int thousendCorrect = palindrom % 10;
-int tenth = palindrom % 10;
+int palindrom = Convert.ToInt32(Console.ReadLine());  // 45658
+int tenHundred = palindrom / 10000;  // 45658/10000 = 4 - первая цифра полиндрома
+int hundred = palindrom / 1000; // 45658 / 1000 = 45
+int hundredCorrect = hundred % 10; // 45%10 = 5 - вторая цифра полиндрома
+int thousend = palindrom / 10; // 45658 / 10 = 4565
+int thousendCorrect = thousend % 10; // 4565%10 = 5 - четвертая цифра полиндрома
+int tenth = palindrom % 10; // 45658%10 = 8 - пятая цифра полиндрома
 
 if (tenHundred == tenth && hundredCorrect == thousendCorrect)
 {
